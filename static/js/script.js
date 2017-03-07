48d16263d2ded0c33beebdafae30860f23034947
Sidstumple <cydstumpel@139.188.028.145.hva.nl>
2017-03-07 16:33:26 +0100

    added detail page

--- static/js/script.js
+++ static/js/script.js
@@ -3,8 +3,9 @@
   var el = {
     search: document.getElementById('getUserQuery'),
     queryResult: document.getElementById('queryResult'),
+    detailSection: document.getElementById('detailResult'),
     error: document.getElementById('error'),
-    button: document.getElementById('button')
+    button: document.getElementById('button'),
   };
 
   var app ={
@@ -14,10 +15,6 @@
         console.log('search was used');
         getData.search();
       });
-      // el.button.addEventListener('click',function(){
-      //   event.preventDefault();
-      //   getData.filter();
-      // });
       routes.init();
     }
   };
@@ -29,11 +26,11 @@
           location.hash = '#search';
         },
         'search': function(){
-          console.log(this.path);
+          console.log('page is ' + this.path);
           sections.toggle('#' + this.path );
         },
         'search/:detail': function(detail) {
-          console.log(this.path);
+          console.log('page is ' + this.path);
           sections.toggle('#' + this.path );
         }
       });
@@ -58,25 +55,33 @@
             console.log('nothing available');
             sections.renderError();
           }
-          console.log(data.Objects);
+          // console.log(data.Objects);
           console.log('search api is loaded');
           // this calls renderSearch and changes the html according to the applied filter
           sections.renderSearch(data);
-
-          // data.Objects.map(function(houses){
-          //   console.log(houses.AantalKamers);
-          //   function getRooms(rooms) {
-          //     return rooms > 3;
-          //   }
-          //   rooms.push(houses.AantalKamers);
-          //   var roomsFilter = rooms.filter(getRooms);
-          //   console.log(roomsFilter);
-          // });
+          sections.renderDetail(data);
         })
     .go()
   },
-  filter: function(){
 
+  detail: function(id) {
+
+      //makes sure api url has the right userquery and adds the value of the selected option
+      var apiUrl = 'http://funda.kyrandia.nl/feeds/Aanbod.svc/json/detail/' + _APIKEY + '/koop/'+ id;
+      // GET data for applied filter
+      console.log(apiUrl);
+      aja()
+      .url(apiUrl)
+      .on('success', function(data){
+        console.log(data);
+        console.log('detail api is loaded');
+        // this calls renderSearch and changes the html according to the applied filter
+        sections.renderDetail(data);
+      })
+  .go()
+  },
+
+  filter: function(){
   }
 };
 
@@ -89,14 +94,35 @@
       var htmlCollection = template(data);
 
       el.queryResult.innerHTML = htmlCollection;
-    },
-    renderError: function() {
-    var source = document.getElementById('errorTemplate').innerHTML;
-    var template = Handlebars.compile(source);
-    var errorHTML = template();
+      //checks when search results are rendered, if a media-item is clicked.
+      var mediaItem = document.querySelectorAll('.house-item');
+      mediaItem.forEach(function(get) {
+        var house = get.id;
+        // console.log(document.getElementById(house));
+        document.getElementById(house).addEventListener('click', function(el) {
+          console.log(this.id);
+          getData.detail(this.id);
+        })
+      })
 
-    el.error.innerHTML = errorHTML;
+    },
+
+    renderError: function() {
+      var source = document.getElementById('errorTemplate').innerHTML;
+      var template = Handlebars.compile(source);
+      var errorHTML = template();
+
+      el.error.innerHTML = errorHTML;
 },
+
+    renderDetail: function(data) {
+      //this is the script template in the html
+      var source = document.getElementById('detailTemplate').innerHTML;
+      var template = Handlebars.compile(source);
+      var htmlDetail = template(data);
+
+      el.detailSection.innerHTML = htmlDetail;
+    },
 
     toggle: function(route) {
       //selects all sections in the document
@@ -104,9 +130,11 @@
       //loop through all sections
       section.forEach(function(section){
         //sectionsId adds a hashtag to all section id's, so it will be the same as the location hash
-        var sectionsId = '#' + section.id;
+        var router = route.slice(1);
+
         //if sectionsId is the same as route remove the hide class
-        if (sectionsId === route) {
+        if (section.id === router || router.slice(8) === section.id) {
+          console.log('remove hide');
           section.classList.remove('hide');
         }else { // else add class hide to section
           section.classList.add('hide');
